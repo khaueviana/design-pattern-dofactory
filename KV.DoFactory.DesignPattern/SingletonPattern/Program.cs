3ad610fc5fd60662939be73ab2ad6534ff66271e
Khauê Viana <khaueviana@gmail.com>
2017-10-25 23:05:15 -0200

    Add Singleton Pattern

---
+++ KV.DoFactory.DesignPattern/SingletonPattern/Program.cs
@@ -0,0 +1,54 @@
+﻿using SingletonPattern.RealWorld;
+using SingletonPattern.StructuralCode;
+using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //STRUCTURAL CODE
+
+            // Constructor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+            // Wait for user
+            Console.ReadKey();
+
+            //REAL WORLD
+
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+
+
+        }
+    }
+}
